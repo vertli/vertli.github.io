52e3b0e8879bf5ea8dade15ced5e45563448f9b4
Calvin Vert Li <vert.c.k.li@gmail.com>
2025-05-09 01:48:11 -0400

    update...
    added title page; added start function; changed size

--- games/colourScheme.js
+++ games/colourScheme.js
@@ -1,5 +1,8 @@
 let c = document.getElementById("gameCanvas");
-let ctx;
+let ctx = null;
+let squareW = 0;
+let squareH = 0;
+let isStarted = false;
 
 const colour = {
   poppyRed: "#DC343B",
@@ -8,64 +11,67 @@ const colour = {
   kashmir: "#6F8D6A",
   dejaVuBlue: "#2E5283",
   damson: "#854C65",
+  empty: "#717388",
 };
 
 let sqrtArray = [];
 
 function init() {
   ctx = c.getContext("2d");
-  c.width = 500;
-  c.height = 200;
+  c.width = Math.min(300, screen.availWidth);
+  squareW = c.width / 5;
+  c.height = Math.min(squareW * 8, screen.availWidth);
+  squareH = c.height / 8;
   c.style = "background:#717388; border:1px solid #000000";
+  titlePage();
+}
+
+function titlePage() {
+  ctx.font = "50px serif";
+  ctx.fillText("COULOR", squareW, squareH * 2, squareW * 3);
+  ctx.fillText("SCHEME", squareW, squareH * 3, squareW * 3);
+  ctx.fillText("> START <", squareW, squareH * 5, squareW * 3);
+}
+
+function startGame() {
+  isStarted = true;
+  ctx.clearRect(0, 0, c.width, c.height);
   createLevel();
 }
 
 function createLevel() {
-  let row1 = [
-    colour.poppyRed,
-    colour.orangeade,
-    colour.lemonGrass,
-    colour.kashmir,
-    colour.dejaVuBlue,
-    colour.damson,
-  ];
-  let row2 = [
-    colour.orangeade,
-    colour.lemonGrass,
-    colour.kashmir,
-    colour.dejaVuBlue,
-    colour.damson,
-    colour.poppyRed,
-  ];
+  let row1 = [colour.dejaVuBlue, colour.damson, colour.kashmir];
+  let row2 = [colour.poppyRed, colour.orangeade, colour.lemonGrass];
+  let row3 = [colour.poppyRed, colour.lemonGrass, colour.kashmir];
+  let row4 = [colour.orangeade, colour.lemonGrass, colour.kashmir];
+  let row5 = [colour.damson, colour.orangeade, colour.dejaVuBlue];
+  let row6 = [colour.orangeade, colour.dejaVuBlue, colour.kashmir];
 
-  sqrtArray.push(row1, row2);
-  console.log(sqrtArray);
+  sqrtArray.push(row1, row2, row3, row4, row5, row6);
 
-  let x = 100;
-  let y = 50;
+  let x = squareW;
+  let y = squareH;
 
-  for (const colour of row1) {
-    drawRect(x, y, 50, 50, colour);
-    x += 50;
-  }
-  x = 100;
-  y += 50;
-  for (const colour of row2) {
-    drawRect(x, y, 50, 50, colour);
-    x += 50;
+  for (const row of sqrtArray) {
+    for (const colour of row) {
+      drawRect(x, y, colour);
+      x += squareW;
+    }
+    x = squareW;
+    y += squareH;
   }
 }
 
-function drawRect(x, y, w, h, colour) {
+function drawRect(x, y, colour) {
   ctx.fillStyle = colour;
-  ctx.fillRect(x, y, w, h);
-  ctx.strokeRect(x, y, w, h);
+  ctx.fillRect(x, y, squareW, squareH);
+  ctx.strokeRect(x, y, squareW, squareH);
   ctx.stroke();
 }
 
 function changeColour(col, row) {
-  if (col < 0 || col > 5 || row < 0 || row > 1) {
-    return;
+  if (col < 0 || col > 2 || row < 0 || row > 5) {
+    return; // out of bounds
   }
   if (sqrtArray[row][col] === colour.poppyRed) {
     sqrtArray[row][col] = colour.orangeade;
@@ -80,19 +86,39 @@ function changeColour(col, row) {
   } else if (sqrtArray[row][col] === colour.damson) {
     sqrtArray[row][col] = colour.poppyRed;
   }
-  drawRect(100 + col * 50, 50 + row * 50, 50, 50, sqrtArray[row][col]);
+  drawRect(
+    squareW + col * squareW,
+    squareH + row * squareH,
+    sqrtArray[row][col]
+  );
 }
 
 c.addEventListener("click", function (event) {
   const xPos = event.clientX - c.getBoundingClientRect().left;
   const yPos = event.clientY - c.getBoundingClientRect().top;
-  console.log("X: " + xPos + " Y: " + yPos);
-  if (xPos < 100 || xPos > 400 || yPos < 50 || yPos > 150) {
+
+  if (!isStarted) {
+    if (
+      xPos > squareW &&
+      xPos < squareW * 4 &&
+      yPos > squareH * 4 &&
+      yPos < squareH * 5
+    ) {
+      startGame();
+    }
     return;
   }
-  const col = (xPos - (xPos % 50)) / 50 - 2;
-  const row = (yPos - (yPos % 50)) / 50 - 1;
-  console.log("Row: " + row + " Col: " + col);
+
+  if (
+    xPos < squareW ||
+    xPos > squareW * 4 ||
+    yPos < squareH ||
+    yPos > squareH * 7
+  ) {
+    return;
+  }
+  const col = (xPos - (xPos % squareW)) / squareW - 1;
+  const row = (yPos - (yPos % squareH)) / squareH - 1;
   changeColour(col, row); // self
   changeColour(col, row - 1); // up
   changeColour(col, row + 1); // down
